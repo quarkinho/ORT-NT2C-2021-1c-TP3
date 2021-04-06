d7ff2d479ef8dd2edd7428ad39e312a472acd18b
lgalant <lgalant@live.com>
2021-04-05 22:47:45 -0300

    Version clases y render

--- script.js
+++ script.js
@@ -12,39 +12,77 @@ const classNames = {
 
   let itemCount = 0;
   let uncheckedCount = 0;
+  let tareas = []
+  class Tarea {
+
+    constructor(titulo) {
+        this.titulo = titulo
+        this.terminada = false;
+    }
+
+   
+   /* funcioncambiobox(eventito) {
+      console.log("en funcioncambiobox this:", this)
+      this.terminada = eventito.target.checked
+      renderizar()
+    }
+*/
+  }
+
+
+  function togglebox(eventito) {
+    console.log("en togglebox this:", this)
+    this.terminada = eventito.target.checked
+    renderizar()
+  }
 
   function addTodo() {
-      const tarea = prompt("Ingrese tarea")
-      const li = document.createElement('li')
+    const titulo = prompt("Ingrese tarea")
+    const tarea = new Tarea(titulo)
+    tareas.push(tarea)
 
-      const cb = document.createElement('input')
-      cb.type = "checkbox"
-      cb.className = classNames.TODO_CHECKBOX
-      cb.onchange = funcioncambiobox
+    renderizar()
 
-      const span = document.createElement('span')
-      span.className = classNames.TODO_TEXT
-      span.innerHTML = tarea
+  }
 
-      li.className = classNames.TODO_ITEM
-      li.appendChild(span)
-      li.appendChild(cb)
-      list.appendChild(li);
+  function renderizar() {
 
-      itemCount++;
-      uncheckedCount++;
-      itemCountSpan.innerHTML = itemCount;
-      uncheckedCountSpan.innerHTML = uncheckedCount;
+    list.innerHTML = ""
+
+    tareas.map((tarea)=> {
+
+
+
+        const cb = document.createElement('input')
+        cb.type = "checkbox"
+        cb.className = classNames.TODO_CHECKBOX
+        cb.checked = tarea.terminada
+        cb.onchange = togglebox.bind(tarea)
+        //cb.onchange = tarea.funcioncambiobox
+  
+        const span = document.createElement('span')
+        span.className = classNames.TODO_TEXT
+        span.innerHTML = tarea.titulo
+        const li = document.createElement('li')
+
+        li.className = classNames.TODO_ITEM
+        li.appendChild(span)
+        li.appendChild(cb)
+        list.appendChild(li);
+
+    })
+
+    itemCountSpan.innerHTML = tareas.length
+    let unchecked = 0
+    tareas.map((t)=> {
+        if (!t.terminada)
+            unchecked++
+    })
+    uncheckedCountSpan.innerHTML = unchecked
+
   }
 
 
-  function funcioncambiobox(eventito) {
 
-      if (eventito.target.checked) {
-        uncheckedCount--;
-      }
-      else
-        uncheckedCount++;
 
-        uncheckedCountSpan.innerHTML = uncheckedCount;
-  }
+   
