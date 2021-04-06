41071eedc4c37b7167e2e928ee07546e039ca864
lgalant <lgalant@live.com>
2021-04-05 21:59:39 -0300

    solucion 1

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
-}
+    TODO_ITEM: 'todo-container',
+    TODO_CHECKBOX: 'todo-checkbox',
+    TODO_TEXT: 'todo-text',
+    TODO_DELETE: 'todo-delete',
+  }
+  
+  
+  const list = document.getElementById('todo-list')
+  const itemCountSpan = document.getElementById('item-count')
+  const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+  let itemCount = 0;
+  let uncheckedCount = 0;
+
+  function addTodo() {
+      const tarea = prompt("Ingrese tarea")
+      const li = document.createElement('li')
+
+      const cb = document.createElement('input')
+      cb.type = "checkbox"
+      cb.className = classNames.TODO_CHECKBOX
+      cb.onchange = funcioncambiobox
+
+      const span = document.createElement('span')
+      span.className = classNames.TODO_TEXT
+      span.innerHTML = tarea
+
+      li.className = classNames.TODO_ITEM
+      li.appendChild(span)
+      li.appendChild(cb)
+      list.appendChild(li);
+
+      itemCount++;
+      uncheckedCount++;
+      itemCountSpan.innerHTML = itemCount;
+      uncheckedCountSpan.innerHTML = uncheckedCount;
+  }
 
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
+  function funcioncambiobox(eventito) {
 
-function addTodo() {
-  alert('Boton Add TODO clickeado!')
-}
+      if (eventito.target.checked) {
+        uncheckedCount--;
+      }
+      else
+        uncheckedCount++;
+
+        uncheckedCountSpan.innerHTML = uncheckedCount;
+  }
